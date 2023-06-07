a59ca68a0b443b1dbea1dbc74324681030d785ed
Richard Neumann <mail@richard-neumann.de>
2023-06-08 01:53:24 +0200

    Order trait implementations alphabetically

--- src/lib.rs
+++ src/lib.rs
@@ -27,26 +27,6 @@ impl Display for Symbol {
     }
 }
 
-impl PartialOrd<Self> for Symbol {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self, other) {
-            (Self::Rock, Self::Paper) => Some(Ordering::Less),
-            (Self::Rock, Self::Scissors) => Some(Ordering::Greater),
-            (Self::Paper, Self::Rock) => Some(Ordering::Greater),
-            (Self::Paper, Self::Scissors) => Some(Ordering::Less),
-            (Self::Scissors, Self::Rock) => Some(Ordering::Less),
-            (Self::Scissors, Self::Paper) => Some(Ordering::Greater),
-            (_, _) => None,
-        }
-    }
-}
-
-impl Ord for Symbol {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap_or(Ordering::Equal)
-    }
-}
-
 impl Distribution<Symbol> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Symbol {
         match rng.gen_range(0..=2) {
@@ -66,6 +46,26 @@ impl FromStr for Symbol {
             "p" => Ok(Self::Paper),
             "s" => Ok(Self::Scissors),
             _ => Err("invalid string"),
+        }
+    }
+}
+
+impl Ord for Symbol {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap_or(Ordering::Equal)
+    }
+}
+
+impl PartialOrd<Self> for Symbol {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Self::Rock, Self::Paper) => Some(Ordering::Less),
+            (Self::Rock, Self::Scissors) => Some(Ordering::Greater),
+            (Self::Paper, Self::Rock) => Some(Ordering::Greater),
+            (Self::Paper, Self::Scissors) => Some(Ordering::Less),
+            (Self::Scissors, Self::Rock) => Some(Ordering::Less),
+            (Self::Scissors, Self::Paper) => Some(Ordering::Greater),
+            (_, _) => None,
         }
     }
 }
