79ec0cc1ad1bb75a66ec8514614785a3162e047c
Richard Neumann <mail@richard-neumann.de>
2025-07-26 15:50:29 +0200

    Re-use buffer across entire game

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,11 @@ const ROUNDS: usize = 3;
 fn main() {
     let mut score: i8 = 0;
     let mut rng = rand::rng();
+    let mut buf = String::new();
 
     for _ in 1..=ROUNDS {
         let computer: Symbol = rng.random();
-        let player = get_player_input();
+        let player = get_player_input(&mut buf);
 
         match player.partial_cmp(&computer) {
             Some(Ordering::Equal) => {
@@ -41,17 +42,16 @@ fn main() {
     }
 }
 
-fn get_player_input() -> Symbol {
+fn get_player_input(buf: &mut String) -> Symbol {
     let mut stdout = stdout().lock();
     let mut stdin = stdin().lock();
-    let mut buf = String::new();
 
     loop {
         write!(stdout, "Your selection (r/p/s): ").expect("Could not write to STDOUT.");
         stdout.flush().expect("Could not flush STDOUT.");
         buf.clear();
 
-        if stdin.read_line(&mut buf).is_ok() {
+        if stdin.read_line(buf).is_ok() {
             match buf.parse() {
                 Ok(rps) => return rps,
                 Err(()) => println!("Invalid input: {buf}"),
